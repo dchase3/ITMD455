153c0549bd08914dd9ecfdb037beea270aec73f9
Dennis Chase <dchase3@hawk.iit.edu>
2018-02-28 20:19:01 -0600

    Fixed timer

--- Lab6/app/src/main/java/com/example/densi/itmd455hw6/BoundServer.java
+++ Lab6/app/src/main/java/com/example/densi/itmd455hw6/BoundServer.java
@@ -3,25 +3,98 @@ package com.example.densi.itmd455hw6;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
-
-/**
- * Created by densi on 2/26/2018.
- */
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class BoundServer extends Service {
+    private static String LOG_TAG = "BoundServer";
+    private IBinder mBinder = new MyBinder();
+    private Chronometer mChronometer;
+    private boolean startStop = false;
 
-    private final IBinder bBinder = new LBinder();
-
-    public class LBinder extends Binder {
-        BoundServer getService() {
-            return BoundServer.this;
-        }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(LOG_TAG, "in onCreate");
+        mChronometer = new Chronometer(this);
+        mChronometer.setBase(SystemClock.elapsedRealtime());
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return bBinder;
+        Log.v(LOG_TAG, "in onBind");
+        return mBinder;
     }
 
+    @Override
+    public void onRebind(Intent intent) {
+        Log.v(LOG_TAG, "in onRebind");
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v(LOG_TAG, "in onUnbind");
+        return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "in onDestroy");
+        mChronometer.stop();
+    }
+
+    public String getTimestamp() {
+        long elapsedMillis = SystemClock.elapsedRealtime()
+                - mChronometer.getBase();
+        int hours = (int) (elapsedMillis / 3600000);
+        int minutes = (int) (elapsedMillis - hours * 3600000) / 60000;
+        int seconds = (int) (elapsedMillis - hours * 3600000 - minutes * 60000) / 1000;
+        int millis = (int) (elapsedMillis - hours * 3600000 - minutes * 60000 - seconds * 1000);
+        return minutes + ":" + seconds + ":" + millis;
+    }
+
+    public void timer(final TextView timeText) {
+        final Handler handle = new Handler();
+        handle.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsedMillis = SystemClock.elapsedRealtime()
+                        - mChronometer.getBase();
+                int hours = (int) (elapsedMillis / 3600000);
+                int minutes = (int) (elapsedMillis - hours * 3600000) / 60000;
+                int seconds = (int) (elapsedMillis - hours * 3600000 - minutes * 60000) / 1000;
+                int millis = (int) (elapsedMillis - hours * 3600000 - minutes * 60000 - seconds * 1000);
+                String time = minutes + ":" + seconds + ":" + millis;
+
+                timeText.setText(time);
+            }
+        });
+    }
+
+    public void startTimer(){
+        mChronometer.start();
+        startStop = true;
+    }
+
+    public void stopTimer(){
+        mChronometer.stop();
+        startStop = false;
+    }
+
+    public void resetTimer(){
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        startStop = false;
+    }
+
+    public class MyBinder extends Binder {
+        BoundServer getService() {
+            return BoundServer.this;
+        }
+    }
 }
--- Lab6/app/src/main/java/com/example/densi/itmd455hw6/MainActivity.java
+++ Lab6/app/src/main/java/com/example/densi/itmd455hw6/MainActivity.java
@@ -1,103 +1,107 @@
 package com.example.densi.itmd455hw6;
-
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.densi.itmd455hw6.BoundServer;
+
 public class MainActivity extends AppCompatActivity {
+    BoundServer mBoundService;
+    boolean mServiceBound = false;
 
-    BoundServer bServer;
-    boolean bBound = false;
-
-    Button startSButton = (Button) findViewById(R.id.startSButton);
-    Button stopSButton = (Button) findViewById(R.id.stopSButton);
-    Button startButton = (Button) findViewById(R.id.startButton);
-    Button stopButton = (Button) findViewById(R.id.stopButton);
-    Button resetButton = (Button) findViewById(R.id.resetButton);
-
-    TextView text = (TextView) findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView timestampText = (TextView) findViewById(R.id.textView);
+        Button startButton = (Button) findViewById(R.id.startButton);
+        Button stopButton = (Button) findViewById(R.id.stopButton);
+        Button resetButton = (Button) findViewById(R.id.resetButton);
+        Button stopServiceButton = (Button) findViewById(R.id.stopSButton);
+        Button startServiceButton = (Button) findViewById(R.id.startSButton);
 
-        startSButton.setOnClickListener(new View.OnClickListener() {
+        startButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!bBound)
-                {
-                    onStart();
-                    text.setText("Start");
+                mBoundService.startTimer();
+                mBoundService.timer(timestampText);
+            }
+        });
+
+        stopButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBoundService.stopTimer();
+            }
+        });
+
+        resetButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBoundService.resetTimer();
+            }
+        });
+
+        startServiceButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStart();
+            }
+        });
+
+        stopServiceButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceBound) {
+                    unbindService(mServiceConnection);
+                    mServiceBound = false;
                 }
+                Intent intent = new Intent(MainActivity.this,
+                        BoundServer.class);
+                stopService(intent);
             }
-        });//Start Services
-
-        stopSButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(bBound)
-                {
-                    onStop();
-                    text.setText("Stop");
-                }
-            }
-        });//Stop Services
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });//Start
-
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });//Stop
-
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });//Reset
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Intent intent = new Intent(this, BoundServer.class);
-        bindService(intent, bConnection, Context.BIND_AUTO_CREATE);
+        startService(intent);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        unbindService(bConnection);
-        bBound = false;
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
     }
 
-    private ServiceConnection bConnection = new ServiceConnection() {
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
         @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            BoundServer.LBinder binder = (BoundServer.LBinder) iBinder;
-            bServer = binder.getService();
-            bBound = true;
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
         }
 
         @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            bBound = false;
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundServer.MyBinder myBinder = (BoundServer.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
         }
     };
 }
